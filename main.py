93c7cc167e63b851af9022400a9b75cb8e10eea0
Kyle <notxap55@yahoo.com>
2024-07-09 20:54:41 -0700

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
-timmy = turtle.Turtle()
-
+timmy = Turtle()
 print(timmy)
+timmy.shape('turtle')
+timmy.color('coral')
+timmy.forward(200)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
